7a95f42d0b62f6442f4feabbbb42d7d92cb13a20
adrianoapi <adrianoapi@hotmail.com>
2019-03-13 10:10:12 -0300

    alter template

--- api/view/footer.php
+++ api/view/footer.php
@@ -1,211 +1,40 @@
- <!-- your content here -->
+</div>
+<!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <div class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                            Copyright � 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                            <div class="text-md-right footer-links d-none d-sm-block">
+                                <a href="javascript: void(0);">About</a>
+                                <a href="javascript: void(0);">Support</a>
+                                <a href="javascript: void(0);">Contact Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- end footer -->
+            <!-- ============================================================== -->
         </div>
-      </div>
-      <footer class="footer">
-        <div class="container-fluid">
-          <nav class="float-left">
-            <ul>
-              <li>
-                <a href="https://www.creative-tim.com">
-                  Creative Tim
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div class="copyright float-right">
-            &copy;
-            <script>
-              document.write(new Date().getFullYear())
-            </script>, made with <i class="material-icons">favorite</i> by
-            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
-          </div>
-          <!-- your footer here -->
-        </div>
-      </footer>
     </div>
-  </div>
-  <!--   Core JS Files   -->
-  <script src="/template/assets/js/core/jquery.min.js"></script>
-  <script src="/template/assets/js/core/popper.min.js"></script>
-  <script src="/template/assets/js/core/bootstrap-material-design.min.js"></script>
-  <script src="https://unpkg.com/default-passive-events"></script>
-  <script src="/template/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!-- Place this tag in your head or just before your close body tag. -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <!-- Chartist JS -->
-  <script src="/template/assets/js/plugins/chartist.min.js"></script>
-  <!--  Notifications Plugin    -->
-  <script src="/template/assets/js/plugins/bootstrap-notify.js"></script>
-  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="/template/assets/js/material-dashboard.js?v=2.1.0"></script>
-  <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-  <script src="/template/assets/demo/demo.js"></script>
-  <script>
-    $(document).ready(function() {
-      $().ready(function() {
-        $sidebar = $('.sidebar');
+    <!-- ============================================================== -->
+    <!-- end main wrapper -->
+    <!-- ============================================================== -->
+    <!-- Optional JavaScript -->
+    <script src="template/assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+    <script src="template/assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="template/assets/vendor/slimscroll/jquery.slimscroll.js"></script>
+    <script src="template/assets/libs/js/main-js.js"></script>
+    
+    <script>
+        jQuery(document).ready(function(){
 
-        $sidebar_img_container = $sidebar.find('.sidebar-background');
-
-        $full_page = $('.full-page');
-
-        $sidebar_responsive = $('body > .navbar-collapse');
-
-        window_width = $(window).width();
-
-        $('.fixed-plugin a').click(function(event) {
-          // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
-          if ($(this).hasClass('switch-trigger')) {
-            if (event.stopPropagation) {
-              event.stopPropagation();
-            } else if (window.event) {
-              window.event.cancelBubble = true;
-            }
-          }
-        });
-
-        $('.fixed-plugin .active-color span').click(function() {
-          $full_page_background = $('.full-page-background');
-
-          $(this).siblings().removeClass('active');
-          $(this).addClass('active');
-
-          var new_color = $(this).data('color');
-
-          if ($sidebar.length != 0) {
-            $sidebar.attr('data-color', new_color);
-          }
-
-          if ($full_page.length != 0) {
-            $full_page.attr('filter-color', new_color);
-          }
-
-          if ($sidebar_responsive.length != 0) {
-            $sidebar_responsive.attr('data-color', new_color);
-          }
-        });
-
-        $('.fixed-plugin .background-color .badge').click(function() {
-          $(this).siblings().removeClass('active');
-          $(this).addClass('active');
-
-          var new_color = $(this).data('background-color');
-
-          if ($sidebar.length != 0) {
-            $sidebar.attr('data-background-color', new_color);
-          }
-        });
-
-        $('.fixed-plugin .img-holder').click(function() {
-          $full_page_background = $('.full-page-background');
-
-          $(this).parent('li').siblings().removeClass('active');
-          $(this).parent('li').addClass('active');
-
-
-          var new_image = $(this).find("img").attr('src');
-
-          if ($sidebar_img_container.length != 0 && $('.switch-sidebar-image input:checked').length != 0) {
-            $sidebar_img_container.fadeOut('fast', function() {
-              $sidebar_img_container.css('background-image', 'url("' + new_image + '")');
-              $sidebar_img_container.fadeIn('fast');
-            });
-          }
-
-          if ($full_page_background.length != 0 && $('.switch-sidebar-image input:checked').length != 0) {
-            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
-
-            $full_page_background.fadeOut('fast', function() {
-              $full_page_background.css('background-image', 'url("' + new_image_full_page + '")');
-              $full_page_background.fadeIn('fast');
-            });
-          }
-
-          if ($('.switch-sidebar-image input:checked').length == 0) {
-            var new_image = $('.fixed-plugin li.active .img-holder').find("img").attr('src');
-            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
-
-            $sidebar_img_container.css('background-image', 'url("' + new_image + '")');
-            $full_page_background.css('background-image', 'url("' + new_image_full_page + '")');
-          }
-
-          if ($sidebar_responsive.length != 0) {
-            $sidebar_responsive.css('background-image', 'url("' + new_image + '")');
-          }
-        });
-
-        $('.switch-sidebar-image input').change(function() {
-          $full_page_background = $('.full-page-background');
-
-          $input = $(this);
-
-          if ($input.is(':checked')) {
-            if ($sidebar_img_container.length != 0) {
-              $sidebar_img_container.fadeIn('fast');
-              $sidebar.attr('data-image', '#');
-            }
-
-            if ($full_page_background.length != 0) {
-              $full_page_background.fadeIn('fast');
-              $full_page.attr('data-image', '#');
-            }
-
-            background_image = true;
-          } else {
-            if ($sidebar_img_container.length != 0) {
-              $sidebar.removeAttr('data-image');
-              $sidebar_img_container.fadeOut('fast');
-            }
-
-            if ($full_page_background.length != 0) {
-              $full_page.removeAttr('data-image', '#');
-              $full_page_background.fadeOut('fast');
-            }
-
-            background_image = false;
-          }
-        });
-
-        $('.switch-sidebar-mini input').change(function() {
-          $body = $('body');
-
-          $input = $(this);
-
-          if (md.misc.sidebar_mini_active == true) {
-            $('body').removeClass('sidebar-mini');
-            md.misc.sidebar_mini_active = false;
-
-            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
-
-          } else {
-
-            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar('destroy');
-
-            setTimeout(function() {
-              $('body').addClass('sidebar-mini');
-
-              md.misc.sidebar_mini_active = true;
-            }, 300);
-          }
-
-          // we simulate the window Resize so the charts will get updated in realtime.
-          var simulateWindowResize = setInterval(function() {
-            window.dispatchEvent(new Event('resize'));
-          }, 180);
-
-          // we stop the simulation of Window Resize after the animations are completed
-          setTimeout(function() {
-            clearInterval(simulateWindowResize);
-          }, 1000);
-
-        });
-      });
-    });
-
- jQuery(document).ready(function(){
-        
         // Cadastrar
         jQuery('#frm_cadastro').submit(function(){
 
@@ -214,14 +43,14 @@
            var valor     = document.getElementById('valor'    ).value;
            var descricao = document.getElementById('descricao').value;
            //var valid = form_validtaion(nome, email);
-           
+
            // Para a aplicação caso haja erro
            /*if(valid.length > 0){
                document.getElementById('msg-erro').innerHTML     = valid.join('<br>');
                document.getElementById('msg-erro').style.display = "block";
                return false;
            }*/
-       
+
            jQuery.ajax({
                type: "POST",
                url:  "./",
@@ -229,7 +58,7 @@
                success: function(data){
 
                    if(data.length > 1){
-                       
+
                        var obj       = JSON.parse(data);
                        var id        = obj['id'       ];
                        var valor     = obj['valor'    ];
@@ -237,7 +66,7 @@
                        var pagamento = obj['pagamento'];
                        var descricao = obj['descricao'];
                        var date      = obj['dt_lancamento'];
-                       
+
                        // Incrementa uma nova linha na tabela
                        $('<tr class="anim highlight"><td><input type="checkbox" name="cadastro[]"></td>' +
                            '<td>' + date      + '</td>' +
@@ -250,7 +79,7 @@
                         .prependTo('#lista_clientes > tbody')
                         .fadeIn("slow")
                         .addClass('normal');
-                       
+
                        /* OLD
                        var table = document.getElementById("lista_clientes");
                        var row   = table.insertRow(0);
@@ -259,37 +88,37 @@
                        var cell3 = row.insertCell(2);
                        var cell4 = row.insertCell(3);
                        var cell5 = row.insertCell(4);
-                       
+
                        cell1.innerHTML = '<input type="checkbox" name="cadastro[]">';
                        cell2.innerHTML = id;
                        cell3.innerHTML = '<a href="javascript:void(0)" id="' + id + '" onClick="edit_row(this.id)">' + nome + '</a>';
                        cell4.innerHTML = email;
                        cell5.innerHTML = '<input type="button" class="btn btn-danger" id="' + id + '" onClick="del_row(this.id,this)" value="Excluir">';
                        */
-                          
+
                        // Emite mensagem de sucesso
                        document.getElementById("frm_cadastro").reset();
                        document.getElementById("msg-success" ).style.display = "block";
-                       
+
                    }else{
                        //location.reload();
                    }
-                   
+
                }
            });
-           
+
         });
-                
+
     });
-    
+
     // Limpar
     function clear_form(){
         document.getElementById("id").value = 0;
         document.getElementById("msg-erro"   ).style.display = "none";
         document.getElementById("msg-success").style.display = "none";
     }
-    
-    
+
+
     // Editar
     function edit_row(id){
 
@@ -311,7 +140,7 @@
         });
 
     }
-    
+
     // Excluir
     function del_row(id, element){
 
@@ -338,14 +167,7 @@
         }
 
     }
-  </script>
-  <script>
-    $(document).ready(function() {
-      // Javascript method's body can be found in assets/js/demos.js
-      md.initDashboardPageCharts();
-
-    });
-  </script>
+    </script>
 </body>
-
+ 
 </html>
--- api/view/lancamento_listagem.php
+++ api/view/lancamento_listagem.php
@@ -1,130 +1,55 @@
 <div class="row">
-                          <div class="col-lg-3 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                              <div class="card-header card-header-success card-header-icon">
-                                <div class="card-icon">
-                                  <i class="material-icons">add_circle</i>
-                                </div>
-                                <p class="card-category">Receita</p>
-                                <h3 class="card-title">R$ <?= number_format($receita[0]['total'], 2, ',', '.') ?></h3>
-                              </div>
-                              <div class="card-footer">
-                                <div class="stats">
-                                  <i class="material-icons text-danger">warning</i>
-                                  <a href="#pablo">Get More Space...</a>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-lg-3 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                              <div class="card-header card-header-warning card-header-icon">
-                                <div class="card-icon">
-                                  <i class="material-icons">payment</i>
-                                </div>
-                                <p class="card-category">Despesa</p>
-                                <h3 class="card-title">R$ <?= number_format($despesa[0]['total'], 2, ',', '.') ?></h3>
-                              </div>
-                              <div class="card-footer">
-                                <div class="stats">
-                                  <i class="material-icons">local_offer</i> Tracked from Google Analytics
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-lg-3 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                              <div class="card-header card-header-rose card-header-icon">
-                                <div class="card-icon">
-                                  <i class="material-icons">swap_horiz</i>
-                                </div>
-                                <p class="card-category">Fluxo</p>
-                                <h3 class="card-title">R$ <?= number_format($receita[0]['total'] - $despesa[0]['total'], 2, ',', '.') ?></h3>
-                              </div>
-                              <div class="card-footer">
-                                <div class="stats">
-                                  <i class="material-icons">date_range</i> Last 24 Hours
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-lg-3 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                              <div class="card-header card-header-danger card-header-icon">
-                                <div class="card-icon">
-                                  <i class="material-icons">save_alt</i>
-                                </div>
-                                <p class="card-category">Perspectiva</p>
-                                <h3 class="card-title">R$ <?= number_format($despesa[0]['total'] - 5400, 2, ',', '.') ?></h3>
-                              </div>
-                              <div class="card-footer">
-                                <div class="stats">
-                                  <i class="material-icons">update</i> Just Updated
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-		<div class="row">
-            <div class="col-md-12">
-              <div class="card">
-                <div class="card-header card-header-primary">
-                  <h4 class="card-title ">Últimos lançamentos</h4>
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-                    <table class="table">
-                      <thead class=" text-primary">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Data</th>
-                                <th scope="col">Grupo</th>
-                                <th scope="col">Descrição</th>
-                                <th scope="col">Valor</th>
-                                <th scope="col">Pgto</th>
-                                <th scope="col">ACAO</th>
-                            </tr>
-                        </thead>
-                      </thead>
-                      <tbody>
-                        <?php 
-                            foreach($lancamentos as $lancamento):
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 
-                                switch ($lancamento['pagamento_id']) {
-                                    case 5:
-                                        $label = 'success';
-                                        break;
-                                    case 1:
-                                        $label = 'primary';
-                                        break;
-                                    default:
-                                        $label = 'danger';
-                                        break;
-                                }
-                        ?>
-                            <tr>
-                                <td><input type="checkbox" name="cadastro[]"></td>
-                                <td><?= Helper::dataToBr($lancamento['dt_lancamento']) ?></td>
-                                <td><?= utf8_encode($lancamento['grupo']) ?></td>
-                                <td><?= utf8_encode($lancamento['descricao' ]) ?></td>
-                                <td class="text-<?=$label?>"><?= $lancamento['valor' ] ?></td>
-                                <td><?= utf8_encode($lancamento['pagamento']) ?></td>
-                                <td>
-                                    <!--<input type="button" class="btn btn-primary " id="<?= $lancamento['id'] ?>" value="E" onClick="edit_row(this.id)">-->
-                                    <input type="button" class="btn btn-danger" id="<?= $lancamento['id'] ?>" value="Excluir" onClick="del_row(this.id,this)">
-                                </td>
-                            </tr>
-                        <?php endforeach;?>
-                        </tbody>
-                        <tfoot>
-                            <tr><td colspan="5"><input type="button" class="btn btn-danger" value="Excluir itens"></td></tr>
-                        </tfoot>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
- 
-            </div>
-          </div>
+
+    <table class="table">
+      <thead class=" text-primary">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Data</th>
+                <th scope="col">Grupo</th>
+                <th scope="col">Descrição</th>
+                <th scope="col">Valor</th>
+                <th scope="col">Pgto</th>
+                <th scope="col">ACAO</th>
+            </tr>
+        </thead>
+      </thead>
+      <tbody>
+        <?php 
+            foreach($lancamentos as $lancamento):
+
+                switch ($lancamento['pagamento_id']) {
+                    case 5:
+                        $label = 'success';
+                        break;
+                    case 1:
+                        $label = 'primary';
+                        break;
+                    default:
+                        $label = 'danger';
+                        break;
+                }
+        ?>
+            <tr>
+                <td><input type="checkbox" name="cadastro[]"></td>
+                <td><?= Helper::dataToBr($lancamento['dt_lancamento']) ?></td>
+                <td><?= utf8_encode($lancamento['grupo']) ?></td>
+                <td><?= utf8_encode($lancamento['descricao' ]) ?></td>
+                <td class="text-<?=$label?>"><?= $lancamento['valor' ] ?></td>
+                <td><?= utf8_encode($lancamento['pagamento']) ?></td>
+                <td>
+                    <!--<input type="button" class="btn btn-primary " id="<?= $lancamento['id'] ?>" value="E" onClick="edit_row(this.id)">-->
+                    <input type="button" class="btn btn-danger" id="<?= $lancamento['id'] ?>" value="Excluir" onClick="del_row(this.id,this)">
+                </td>
+            </tr>
+        <?php endforeach;?>
+        </tbody>
+        <tfoot>
+            <tr><td colspan="5"><input type="button" class="btn btn-danger" value="Excluir itens"></td></tr>
+        </tfoot>
+    </table>
+
+</div>
+</div>
